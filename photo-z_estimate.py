87e0049dbad00536013d50204b27de04dbb3f378
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2026-03-31 11:08:35 +0200

    Loaded datasets inside code

--- photo-z_estimate.py
+++ photo-z_estimate.py
@@ -1,3 +1,22 @@
 import numpy as np
+import pandas as pd
 
-#Caricamet
+#Caricamento dei dataset
+train_noCS = pd.read_csv("Data/train_noCS.csv")
+test_noCS = pd.read_csv("Data/test_noCS.csv")
+
+train_weakCS = pd.read_csv("Data/train_weakCS.csv")
+test_weakCS = pd.read_csv("Data/test_weakCS.csv")
+
+train_mildCS = pd.read_csv("Data/train_mildCS.csv")
+test_mildCS = pd.read_csv("Data/test_mildCS.csv")
+
+train_strongCS = pd.read_csv("Data/train_strongCS.csv")
+test_strongCS = pd.read_csv("Data/test_strongCS.csv")
+
+print(train_noCS)
+print(test_noCS)
+
+print(train_weakCS)
+print(test_weakCS)
+
